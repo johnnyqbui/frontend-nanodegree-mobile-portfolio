bb3ab776124c49c48ea69795ebf8d0a9cc036bfb
johnnyqbui <johnnyqbui7@gmail.com>
2016-06-04 03:20:21 -0500

    Inlined Css

--- views/js/main.js
+++ views/js/main.js
@@ -531,7 +531,7 @@ function updatePositions() {
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
-// // Generates the sliding pizzas when the page loads.
+// Generates the sliding pizzas when the page loads.
 // document.addEventListener('DOMContentLoaded', function() {
 
 // });
