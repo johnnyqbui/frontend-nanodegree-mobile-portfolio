5350a49b63b8733c093d3c49c5ecaa2795dd4c3e
johnnyqbui <johnnyqbui7@gmail.com>
2016-06-04 21:49:01 -0500

    removed useless css classes, compressed images, updated README file

--- views/js/main.js
+++ views/js/main.js
@@ -376,7 +376,7 @@ var pizzaElementGenerator = function(i) {
   pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
   pizzaImageContainer.classList.add("col-md-6");
 
-  pizzaImage.src = "images/pizza.png";
+  pizzaImage.src = "images/pizza-min.png";
   pizzaImage.classList.add("img-responsive");
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
@@ -484,7 +484,7 @@ function slidingPizzas() {
   for (var i = 0; i < 22; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza-min.png";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
